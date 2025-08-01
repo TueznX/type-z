9c441f12e3c47b57c9feecf2776fad48e813e5c7
TueznX <101438471+TueznX@users.noreply.github.com>
2025-08-01 18:43:49 -0300

    Add files via upload

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,3 +1,10 @@
+// Quando o HTML estiver pronto (antes de carregar imagens e outros recursos)
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingScreen = document.querySelector(".loadingscreen");
+  loadingScreen.classList.add("visible");
+});
+
+// Quando a página estiver 100% carregada (imagens, CSS, etc.)
 window.addEventListener("load", () => {
   const loadingScreen = document.querySelector(".loadingscreen");
   loadingScreen.classList.remove("visible");
